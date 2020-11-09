dc1c93ef4cd21f1090fdde9aa125b1e330140926
bobrovsi1988 <bobrovsi1988@gmail.com>
2020-11-10 01:21:51 +0300

    feat:add reset continue , save

--- gem-puzzle/script.js
+++ gem-puzzle/script.js
@@ -1,10 +1,15 @@
 const Gem ={
 properties :{
-    size : 4,
+    size : 3,
     countMove : 0,
+    continue:false,
 },
 init(){    
     document.body.appendChild(this.countMoveEl()),
+    document.body.appendChild(this._reset()),
+    document.body.appendChild(this._save()),
+    document.body.appendChild(this._continue()),
+
     document.body.appendChild(this.createArena(this.properties.size))
     
      
@@ -17,6 +22,52 @@ countMoveEl(){
     count.textContent=this.properties.countMove;
     return count;
 },
+_reset(){
+let button = document.createElement("button");
+button.classList.add("button-reset");
+button.textContent = "reset";
+button.addEventListener("click", ()=>{
+    document.body.innerHTML="";
+    this.properties.countMove = 0;
+    this.init();
+});
+return button;
+},
+_continue(){
+let button = document.createElement("button");
+button.classList.add("button-reset");
+button.textContent = "continue";
+button.addEventListener("click", ()=>{
+    this.properties.continue = !this.properties.continue;
+    let arena =document.getElementById("arena").innerHTML = localStorage.getItem("arrSave");
+    this.properties.countMove = localStorage.getItem("counts");
+    this.properties.size = +localStorage.getItem("size");
+    document.body.innerHTML="";
+    this.init();
+    //this.init();
+});
+return button;
+},
+_save(){
+    let button = document.createElement("button");
+button.classList.add("button-reset");
+button.textContent = "save";
+button.addEventListener("click", ()=>{
+    let arr =[];
+    let arena =document.querySelectorAll(".wrap");
+    
+    Array.from(arena).forEach(el=>{
+        el.firstChild === null? arr.push(null):arr.push(el.firstChild.textContent)
+
+    })
+    console.log(arr);
+    localStorage.setItem("arrSave", arr);
+    localStorage.setItem("counts", this.properties.countMove);
+    localStorage.setItem("size", this.properties.size);
+});
+return button;
+    
+},
 createArena(sizeArena){
     let numGems = Math.pow(sizeArena, 2);
     let arena =document.createElement("div");
@@ -26,6 +77,7 @@ createArena(sizeArena){
     arena.style.display = 'grid';
     arena.style.height = sizeArena* gemsize+"px";
     arena.style.width = sizeArena*gemsize+"px";
+    arena.setAttribute("id", "arena");
     arena.classList.add("arena");
     
     for(let i =1;i<numGems;i++){
@@ -34,7 +86,12 @@ createArena(sizeArena){
     arr.sort(() => Math.random() - 0.5);
     //console.log(arr);
 
+    if(this.properties.continue){
+        arr = localStorage.getItem("arrSave").split(",");
+        this.properties.continue=!this.properties.continue;
     
+    }
+    console.log(arr);
     for(let i=0; i<numGems; i++){
         let gem = document.createElement("div");
         let wrapper = document.createElement("div");
@@ -42,7 +99,8 @@ createArena(sizeArena){
         wrapper.setAttribute("id" , i+1); 
         gem.classList.add("gem");
         gem.textContent = arr[i];
-        if(i<numGems-1){wrapper.appendChild(gem);}
+        //if(i<numGems-1){wrapper.appendChild(gem);}
+        if(arr[i]){wrapper.appendChild(gem);}
         arena.appendChild(wrapper);
 
         gem.addEventListener("click",()=>{ 
@@ -78,17 +136,17 @@ _movebyclick(idWrapper){
     idWrapper =+idWrapper;
     let pos = idWrapper%this.properties.size;
     let thatEl = document.getElementById(idWrapper);
-    let up = document.getElementById(idWrapper-4);
-    let down = document.getElementById(idWrapper+4);
+    let up = document.getElementById(idWrapper-this.properties.size);
+    let down = document.getElementById(idWrapper+this.properties.size);
     let left = document.getElementById(idWrapper-1);
     let rigt = document.getElementById(idWrapper+1);
-    
+    console.log("xxxxxxxx   "+down + "yyyy" + idWrapper);
     move(down);
     move(up);
     if(pos!==1){move(left);}
     if(pos!==0){move(rigt);}
     
-    console.log();
+    
 
 }, 
 _victory(){
